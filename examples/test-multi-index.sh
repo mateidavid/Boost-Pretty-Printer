e5fa1ef4428eae777b86c0092a14ea17215568b7
Matei David <matei@cs.toronto.edu>
2014-07-17 16:52:41 -0400

    messages to stderr

--- examples/test-multi-index.sh
+++ examples/test-multi-index.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ -x test-multi-index ] && [ test-multi-index -nt test-multi-index.cpp ]; then
-    echo "test-multi-index: exists"
+    echo "test-multi-index: exists" >&2
 else
-    echo "test-multi-index: compiling"
+    echo "test-multi-index: compiling" >&2
     g++ -g -std=c++0x -Wall -Wextra -o test-multi-index test-multi-index.cpp ||
     {
-        echo "compilation failed"
+        echo "compilation failed" >&2
         exit 1
     }
 fi
-echo "test-multi-index: invoking gdb"
+echo "test-multi-index: invoking gdb" >&2
 gdb test-multi-index -x test-multi-index.gdb
--- examples/test-other.sh
+++ examples/test-other.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ -x test-other ] && [ test-other -nt test-other.cpp ]; then
-    echo "test-other: exists"
+    echo "test-other: exists" >&2
 else
-    echo "test-other: compiling"
+    echo "test-other: compiling" >&2
     g++ -g -std=c++0x -Wall -Wextra -o test-other test-other.cpp ||
     {
-        echo "compilation failed"
+        echo "compilation failed" >&2
         exit 1
     }
 fi
-echo "test-other: invoking gdb"
+echo "test-other: invoking gdb" >&2
 gdb test-other -x test-other.gdb
